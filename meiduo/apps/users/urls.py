443f030735a4bb3e848bbc100b8ce60e8bfb02a7
listen <listen24352@163.com>
2025-07-20 19:04:20 +0800

    url

--- meiduo/apps/users/urls.py
+++ meiduo/apps/users/urls.py
@@ -1,34 +1,9 @@
-# from django.urls import path
-# # from apps.users.views.register_views import UsernameCountView, RegisterView, LoginView, LogoutView, CenterView, UserHistoryView
-# # from apps.users.views import EmailView, EmailVerifyView, AddressCreateView, AddressView
-#
-# urlpatterns = {
-#     path('usernames/<str:username>/count/', UsernameCountView.as_view(), name='username_count'),
-#     # 注册
-#     path('register/', RegisterView.as_view()),
-#     # 登录
-#     path('login/', LoginView.as_view()),
-#     # 退出
-#     path('logout/', LogoutView.as_view()),
-#     # 用户中心
-#     path('info/', CenterView.as_view()),
-#     # 邮件保存
-#     path('emails/', EmailView.as_view()),
-#     # 邮件验证
-#     path('emails/verification/', EmailVerifyView.as_view()),
-#     # 新增地址
-#     path('addresses/create/', AddressCreateView.as_view()),
-#     # 查询地址
-#     path('addresses/', AddressView.as_view()),
-#     # 浏览记录
-#     path('browse_histories/', UserHistoryView.as_view()),
-# }
-
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from apps.users.views.register_views import UserRegisterViewSet
 from apps.users.views.login_views import LoginView
+from apps.users.views.views import LogoutView, CenterView, EmailView, EmailVerifyView, AddressCreateView, AddressView, \
+    UserHistoryView
 
 router = DefaultRouter()  # DRF 提供的自动路由生成器，可根据视图集自动生成 URL。
 router.register(r'users', UserRegisterViewSet, basename='user_register')  # 将视图集注册到路由中
@@ -43,4 +18,17 @@ urlpatterns = [
     # 用户注册: /api/users/
     path('', include(router.urls)),
     path('login/', LoginView.as_view(), name='user_login'),
+    path('logout/', LogoutView.as_view()),
+    # 用户中心
+    path('info/', CenterView.as_view()),
+    # 邮件保存
+    path('emails/', EmailView.as_view()),
+    # 邮件验证
+    path('emails/verification/', EmailVerifyView.as_view()),
+    # 新增地址
+    path('addresses/create/', AddressCreateView.as_view()),
+    # 查询地址
+    path('addresses/', AddressView.as_view()),
+    # 浏览记录
+    path('browse_histories/', UserHistoryView.as_view()),
 ]
